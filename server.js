d3c0749294b1862b8b8e2141e1472f8cfed847a1
lukasimonishvili <simonishvililuka@gmail.com>
2020-04-16 18:59:56 +0400

    heroku debug

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "build")));
 
 app.all("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/airLink/index.html"));
+  res.sendFile(path.join(__dirname, "build/index.html"));
 });
 
 const port = process.env.PORT || "3000";

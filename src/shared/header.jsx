ec2422834bf52f9d05f41a8614ca7185ebf0ad53
lukasimonishvili <simonishvililuka@gmail.com>
2020-04-19 01:20:17 +0400

    navigation

--- src/shared/header.jsx
+++ src/shared/header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Link } from "@reach/router";
 
@@ -60,25 +60,151 @@ const BookNow = styled.button`
   cursor: pointer;
 `;
 
+const Navigation = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.86);
+  position: fixed;
+  left: 0;
+  top: 0;
+  transition: 0.3s;
+  transform: ${(props) => (props.visible === true ? "scale(1)" : "scale(0)")};
+  z-index: 100;
+`;
+
+const Close = styled.div`
+  width: 40px;
+  height: 40px;
+  margin-left: auto;
+  margin-top: 90px;
+  cursor: pointer;
+
+  & div {
+    width: 100%;
+    height: 2px;
+    background-color: white;
+
+    &:first-child {
+      transform: rotate3d(0, 0, 1, 45deg);
+    }
+
+    &:last-child {
+      transform: rotate3d(0, 0, 1, -45deg);
+    }
+  }
+`;
+
+const List = styled.ul`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const ListItem = styled.li`
+  color: white;
+  font-size: 3.6rem;
+  line-height: 4.8rem;
+  padding: 22px 0;
+  border-bottom: 1px solid transparent;
+  transition: 0.3s;
+  text-align: center;
+
+  &:hover {
+    border-color: white;
+  }
+`;
+
 const Header = (props) => {
+  const [visible, setVisible] = useState(false);
+
   return (
-    <Container type="header">
-      <Wrapper>
-        <LeftWrapper>
-          <Burger>
-            <BurgerItem size="large" />
-            <BurgerItem size="small" />
-            <BurgerItem size="large" />
-            <BurgerItem size="small" />
-          </Burger>
-          <Lang>EN</Lang>
-        </LeftWrapper>
-        <Link to="/">
-          <Logo alt="" src={LogoImage} />
-        </Link>
-        <BookNow>BOOK NOW</BookNow>
-      </Wrapper>
-    </Container>
+    <React.Fragment>
+      <Container type="header">
+        <Wrapper>
+          <LeftWrapper>
+            <Burger
+              onClick={() => {
+                setVisible(true);
+              }}
+            >
+              <BurgerItem size="large" />
+              <BurgerItem size="small" />
+              <BurgerItem size="large" />
+              <BurgerItem size="small" />
+            </Burger>
+            <Lang>EN</Lang>
+          </LeftWrapper>
+          <Link to="/">
+            <Logo alt="" src={LogoImage} />
+          </Link>
+          <BookNow>BOOK NOW</BookNow>
+        </Wrapper>
+      </Container>
+      <Navigation visible={visible}>
+        <Container>
+          <Close
+            onClick={() => {
+              setVisible(false);
+            }}
+          >
+            <div></div>
+            <div></div>
+          </Close>
+          <List>
+            <ListItem
+              onClick={() => {
+                setVisible(false);
+              }}
+            >
+              <Link to="/about">About us</Link>
+            </ListItem>
+            <ListItem
+              onClick={() => {
+                setVisible(false);
+              }}
+            >
+              <Link to="/gallery">Gallery</Link>
+            </ListItem>
+            <ListItem
+              onClick={() => {
+                setVisible(false);
+              }}
+            >
+              <Link to="/Rooms">Rooms</Link>
+            </ListItem>
+            <ListItem
+              onClick={() => {
+                setVisible(false);
+              }}
+            >
+              <Link to="/Restaurants">Restaurants</Link>
+            </ListItem>
+            <ListItem
+              onClick={() => {
+                setVisible(false);
+              }}
+            >
+              <Link to="/wine">Wine Garage</Link>
+            </ListItem>
+            <ListItem
+              onClick={() => {
+                setVisible(false);
+              }}
+            >
+              <Link to="/location">Location</Link>
+            </ListItem>
+            <ListItem
+              onClick={() => {
+                setVisible(false);
+              }}
+            >
+              <Link to="/contact">Contact</Link>
+            </ListItem>
+          </List>
+        </Container>
+      </Navigation>
+    </React.Fragment>
   );
 };
 
